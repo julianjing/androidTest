faaad2427565bf8ce0b4cd72a633b47454f41733
julianjing <julian.jing09@gmail.com>
2013-06-13 10:15:53 +0800

    add trikmarks

--- src/com/android/test/PageViewerTest.java
+++ src/com/android/test/PageViewerTest.java
@@ -1,20 +1,31 @@
 package com.android.test;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.widget.RadioGroup;
 
 import com.android.test.adapters.DemoCollectionPagerAdapter;
 
 public class PageViewerTest extends FragmentActivity {
-    // When requested, this adapter returns a DemoObjectFragment,
+    protected static final String TAG = PageViewerTest.class.getName();
+	// When requested, this adapter returns a DemoObjectFragment,
     // representing an object in the collection.
     DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
     ViewPager mViewPager;
 
     public void onCreate(Bundle savedInstanceState) {
+    	final ActionBar actionBar = getActionBar();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pageviewertest);
+        
+        final RadioGroup tickmarks = (RadioGroup) findViewById(R.id.tickmarks);
+        
+        
 
         // ViewPager and its adapters use support library
         // fragments, so use getSupportFragmentManager.
@@ -23,6 +34,68 @@ public class PageViewerTest extends FragmentActivity {
                         getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+        mViewPager.setOnPageChangeListener(
+                new ViewPager.SimpleOnPageChangeListener() {
+                    @Override
+                    public void onPageSelected(int position) {
+                        // When swiping between pages, select the
+                        // corresponding tab.
+                        getActionBar().setSelectedNavigationItem(position);
+                        Log.d(TAG, "tickmarks check:"+position);         
+                        tickmarks.check(getTickmarksId(position));
+                    }
+
+					private int getTickmarksId(int position) {
+						switch(position) {
+							case 0:
+								return R.id.radio0;
+							case 1:
+								return R.id.radio1;
+							case 2: 
+								return R.id.radio2;
+							case 3: 
+								return R.id.radio3;
+						}
+						return -1;
+					}
+                });
+
+        
+     // Specify that tabs should be displayed in the action bar.
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Create a tab listener that is called when the user changes tabs.
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+
+			@Override
+			public void onTabReselected(Tab arg0,
+					android.app.FragmentTransaction arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTabSelected(Tab arg0,
+					android.app.FragmentTransaction arg1) {
+				 mViewPager.setCurrentItem(arg0.getPosition());
+			}
+
+			@Override
+			public void onTabUnselected(Tab arg0,
+					android.app.FragmentTransaction arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+        };
+
+        // Add 3 tabs, specifying the tab's text and TabListener
+        for (int i = 0; i < 4; i++) {
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText("Tab " + (i + 1))
+                            .setTabListener(tabListener));
+        }
+
     }
 }
 
